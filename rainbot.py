37ae15afff6317e996f167ca2e277e1550eb3ab9
ClementWalter <clement0walter@gmail.com>
2018-11-28 00:01:47 +0100

    Update starting server in main

--- rainbot.py
+++ rainbot.py
@@ -104,6 +104,6 @@ def book_tennis_court():
 
 
 if __name__ == '__main__':
-    scheduler.start()
     PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+    scheduler.start()
