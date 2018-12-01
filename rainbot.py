95a405b669fa81beb3a134bf6c744e4b1923faa8
ClementWalter <clement0walter@gmail.com>
2018-12-01 21:20:51 +0100

    Add scheduler creators

--- rainbot.py
+++ rainbot.py
@@ -11,95 +11,113 @@ logging.basicConfig()
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 PARIS_TENNIS_URL = 'https://tennis.paris.fr/tennis/jsp/site/Portal.jsp'
-DAY_OF_WEEK = os.getenv('DAY_OF_WEEK', 'tue')
+DAYS_OF_BOOKING = os.getenv('DAYS_OF_BOOKING', 'tue').split(',')
+USERNAMES = os.getenv('USERNAME', '').split(',')
+PASSWORDS = os.getenv('PASSWORD', '').split(',')
 HOUR = int(os.getenv('HOUR', 8))
 MINUTE = int(os.getenv('MINUTE', 2))
 JITTER = int(os.getenv('JITTER', 100))
+HOUR_FROM = int(os.getenv('HOUR_FROM', 20))
+HOUR_TO = int(os.getenv('HOUR_TO', 22))
+TENNIS_LIST = os.getenv('TENNIS_LIST', 'Bertrand Dauvin,Jules Ladoumègue,Docteurs Déjerine,Sept Arpents').split(',')
+IN_OUT = os.getenv('IN_OUT', 'V').split(',')  # V = couvert, F = non couvert
 
-scheduler = BlockingScheduler()
+
+def create_booking_job(username, password):
+    def book_tennis_court():
+        # Login request
+        session = requests.session()
+        response = session.get('https://moncompte.paris.fr/moncompte/jsp/site/Portal.jsp')
+        soup = BeautifulSoup(response.text, features='html5lib')
+        token_input = soup.find('input', {'name': 'token'})
+        token = token_input.attrs['value']
+        login_data = {
+            'page': 'mylutece',
+            'action': 'dologin',
+            'token': token,
+            'auth_provider': 'mylutece-openam',
+            'username': username,
+            'password': password,
+            'Submit': '',
+        }
+        session.post('https://moncompte.paris.fr/moncompte/jsp/site/plugins/mylutece/DoMyLuteceLogin.jsp', login_data)
+
+        # Find time spot
+        booking_date = (datetime.datetime.now() + datetime.timedelta(days=6)).strftime('%d/%m/%Y')
+        search_data = {
+            'hourRange': f'{HOUR_FROM}-{HOUR_TO}',
+            'when': booking_date,
+            'selWhereTennisName': TENNIS_LIST,
+            'selCoating': ['96', '2095', '94', '1324', '2016', '92'],
+            'selInOut': IN_OUT,
+        }
+        response = session.post(
+            PARIS_TENNIS_URL,
+            search_data,
+            params={'page': 'recherche', 'action': 'rechercher_creneau'}
+        )
+        soup = BeautifulSoup(response.text, features='html5lib')
+        courts = soup.findAll('button', {'class': 'buttonAllOk'})
+
+        if not courts:
+            return logging.log(logging.WARNING, f'No court available on {booking_date}')
+
+        reservation_data = {
+            'equipmentId': courts[0].attrs['equipmentid'],
+            'courtId': courts[0].attrs['courtid'],
+            'dateDeb': courts[0].attrs['datedeb'],
+            'dateFin': courts[0].attrs['datefin'],
+            'annulation': False
+        }
+        session.post(
+            PARIS_TENNIS_URL,
+            reservation_data,
+            params={'page': 'reservation', 'view': 'reservation_creneau'}
+        )
+        player_data = {
+            'player1': ['Roger', 'Federer', ''],
+            'counter': '',
+            'submitControle': 'submit'
+        }
+        session.post(
+            PARIS_TENNIS_URL,
+            player_data,
+            params={'page': 'reservation', 'action': 'validation_court'}
+        )
+
+        # Page paiement
+        response = session.get(
+            PARIS_TENNIS_URL,
+            params={'page': 'reservation', 'view': 'methode_paiement'}
+        )
+        soup = BeautifulSoup(response.text, features='html5lib')
+        if soup.find('table', {'nbtickets': 10}):
+            return logging.log(
+                logging.WARNING,
+                'Insufficient credit to proceed with payment. Reservation on hold for 15 minutes.'
+            )
+
+        payment_data = {
+            'page': 'reservation',
+            'action': 'selection_methode_paiement',
+            'paymentMode': 'existingTicket',
+            'nbTickets': '1',
+        }
+        session.post(PARIS_TENNIS_URL, payment_data)
+        return logging.log(logging.INFO, 'Court successfully booked')
+
+    return book_tennis_court
 
 
-@scheduler.scheduled_job('cron', day_of_week=DAY_OF_WEEK, hour=HOUR, minute=MINUTE, jitter=JITTER)
-def book_tennis_court():
-    # Login request
-    session = requests.session()
-    response = session.get('https://moncompte.paris.fr/moncompte/jsp/site/Portal.jsp')
-    soup = BeautifulSoup(response.text, features='html5lib')
-    token_input = soup.find('input', {'name': 'token'})
-    token = token_input.attrs['value']
-    login_data = {
-        'page': 'mylutece',
-        'action': 'dologin',
-        'token': token,
-        'auth_provider': 'mylutece-openam',
-        'username': os.getenv('USERNAME'),
-        'password': os.getenv('PASSWORD'),
-        'Submit': '',
-    }
-    session.post('https://moncompte.paris.fr/moncompte/jsp/site/plugins/mylutece/DoMyLuteceLogin.jsp', login_data)
-
-    # Find time spot
-    booking_date = (datetime.datetime.now() + datetime.timedelta(days=6)).strftime('%d/%m/%Y')
-    search_data = {
-        'hourRange': '20-22',
-        'when': booking_date,
-        'selWhereTennisName': ['Bertrand Dauvin', 'Jules Ladoumègue', 'Docteurs Déjerine', 'Sept Arpents'],
-        'selCoating': ['96', '2095', '94', '1324', '2016', '92'],
-        'selInOut': ['V', 'F'],  # V = couvert, F = non couvert
-    }
-    response = session.post(
-        PARIS_TENNIS_URL,
-        search_data,
-        params={'page': 'recherche', 'action': 'rechercher_creneau'}
-    )
-    soup = BeautifulSoup(response.text, features='html5lib')
-    courts = soup.findAll('button', {'class': 'buttonAllOk'})
-
-    if not courts:
-        return logging.log(logging.WARNING, f'No court available on {booking_date}')
-
-    reservation_data = {
-        'equipmentId': courts[0].attrs['equipmentid'],
-        'courtId': courts[0].attrs['courtid'],
-        'dateDeb': courts[0].attrs['datedeb'],
-        'dateFin': courts[0].attrs['datefin'],
-        'annulation': False
-    }
-    session.post(
-        PARIS_TENNIS_URL,
-        reservation_data,
-        params={'page': 'reservation', 'view': 'reservation_creneau'}
-    )
-    player_data = {
-        'player1': ['Roger', 'Federer', ''],
-        'counter': '',
-        'submitControle': 'submit'
-    }
-    session.post(
-        PARIS_TENNIS_URL,
-        player_data,
-        params={'page': 'reservation', 'action': 'validation_court'}
-    )
-
-    # Page paiement
-    response = session.get(
-        PARIS_TENNIS_URL,
-        params={'page': 'reservation', 'view': 'methode_paiement'}
-    )
-    soup = BeautifulSoup(response.text, features='html5lib')
-    if soup.find('table', {'nbtickets': 10}):
-        return logging.log(logging.WARNING,
-            'Insufficient credit to proceed with payment. Reservation on hold for 15 minutes.')
-
-    payment_data = {
-        'page': 'reservation',
-        'action': 'selection_methode_paiement',
-        'paymentMode': 'existingTicket',
-        'nbTickets': '1',
-    }
-    session.post(PARIS_TENNIS_URL, payment_data)
-    return logging.log(logging.INFO, 'Court successfully booked')
+def create_scheduler():
+    scheduler = BlockingScheduler()
+    for username, password, day_of_booking in zip(USERNAMES, PASSWORDS, DAYS_OF_BOOKING):
+        scheduler.add_job(
+            create_booking_job(username, password),
+            'cron', day_of_week=day_of_booking, hour=HOUR, minute=MINUTE, jitter=JITTER,
+        )
+    return scheduler
 
 
 if __name__ == '__main__':
-    scheduler.start()
+    create_scheduler().start()
