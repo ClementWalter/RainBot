ce5df55eb1e644396bd28ce387887c9eac7de1da
ClementWalter <clement0walter@gmail.com>
2019-01-06 22:10:22 +0100

    Remove hard coded info

--- rainbot.py
+++ rainbot.py
@@ -10,26 +10,29 @@ from bs4 import BeautifulSoup
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
-PARIS_TENNIS_URL = 'https://tennis.paris.fr/tennis/jsp/site/Portal.jsp'
+# Site info
+LOGIN_URL = os.getenv('LOGIN_URL')
+BOOKING_URL = os.getenv('BOOKING_URL')
 # Cron info
-DAYS_OF_BOOKING = os.getenv('DAYS_OF_BOOKING', 'tue').split(',')
-HOUR = int(os.getenv('HOUR', 7))
-MINUTE = int(os.getenv('MINUTE', 2))
-JITTER = int(os.getenv('JITTER', 100))
-# User info
+DAYS_OF_BOOKING = os.getenv('DAYS_OF_BOOKING').split(',')
+HOUR = int(os.getenv('HOUR'))
+MINUTE = int(os.getenv('MINUTE'))
+SECOND = int(os.getenv('SECOND'))
+JITTER = int(os.getenv('JITTER'))
+# Users info
 USERNAMES = os.getenv('USERNAMES', '').split(',')
 PASSWORDS = os.getenv('PASSWORDS', '').split(',')
-HOUR_FROM = int(os.getenv('HOUR_FROM', 20))
-HOUR_TO = int(os.getenv('HOUR_TO', 22))
-TENNIS_LIST = os.getenv('TENNIS_LIST', 'Sept Arpents,Bertrand Dauvin,Jules Ladoumègue,Docteurs Déjerine').split(',')
+HOUR_FROM = int(os.getenv('HOUR_FROM'))
+HOUR_TO = int(os.getenv('HOUR_TO'))
+PLACES_LIST = os.getenv('PLACES_LIST').split(',')
 IN_OUT = os.getenv('IN_OUT', 'V').split(',')  # V = couvert, F = non couvert
 
 
 def create_booking_job(username, password):
-    def book_tennis_court():
+    def book_court():
         # Login request
         session = requests.session()
-        response = session.get('https://moncompte.paris.fr/moncompte/jsp/site/Portal.jsp')
+        response = session.get('%sPortal.jsp' % LOGIN_URL)
         soup = BeautifulSoup(response.text, features='html5lib')
         token_input = soup.find('input', {'name': 'token'})
         token = token_input.attrs['value']
@@ -42,19 +45,19 @@ def create_booking_job(username, password):
             'password': password,
             'Submit': '',
         }
-        session.post('https://moncompte.paris.fr/moncompte/jsp/site/plugins/mylutece/DoMyLuteceLogin.jsp', login_data)
+        session.post('%splugins/mylutece/DoMyLuteceLogin.jsp' % LOGIN_URL, login_data)
 
         # Find time spot
         booking_date = (datetime.datetime.now() + datetime.timedelta(days=6)).strftime('%d/%m/%Y')
         search_data = {
             'hourRange': f'{HOUR_FROM}-{HOUR_TO}',
             'when': booking_date,
-            'selWhereTennisName': TENNIS_LIST,
+            'selWhereTennisName': PLACES_LIST,
             'selCoating': ['96', '2095', '94', '1324', '2016', '92'],
             'selInOut': IN_OUT,
         }
         response = session.post(
-            PARIS_TENNIS_URL,
+            BOOKING_URL,
             search_data,
             params={'page': 'recherche', 'action': 'rechercher_creneau'}
         )
@@ -81,7 +84,7 @@ def create_booking_job(username, password):
             'annulation': False
         }
         response = session.post(
-            PARIS_TENNIS_URL,
+            BOOKING_URL,
             reservation_data,
             params={'page': 'reservation', 'view': 'reservation_creneau'}
         )
@@ -93,7 +96,7 @@ def create_booking_job(username, password):
             'submitControle': 'submit'
         }
         response = session.post(
-            PARIS_TENNIS_URL,
+            BOOKING_URL,
             player_data,
             params={'page': 'reservation', 'action': 'validation_court'}
         )
@@ -102,7 +105,7 @@ def create_booking_job(username, password):
 
         # Payment page
         response = session.get(
-            PARIS_TENNIS_URL,
+            BOOKING_URL,
             params={'page': 'reservation', 'view': 'methode_paiement'}
         )
         if response.status_code >= 400:
@@ -121,10 +124,10 @@ def create_booking_job(username, password):
             'paymentMode': 'existingTicket',
             'nbTickets': '1',
         }
-        session.post(PARIS_TENNIS_URL, payment_data)
+        session.post(BOOKING_URL, payment_data)
         return logging.log(logging.INFO, f'Court successfully booked for {username}')
 
-    return book_tennis_court
+    return book_court
 
 
 def create_scheduler():
@@ -133,7 +136,7 @@ def create_scheduler():
         logging.log(logging.INFO, f'Creating booking job for {username} on {day_of_booking}')
         scheduler.add_job(
             create_booking_job(username, password),
-            'cron', day_of_week=day_of_booking, hour=HOUR, minute=MINUTE, jitter=JITTER,
+            'cron', day_of_week=day_of_booking, hour=HOUR, minute=MINUTE, second=SECOND, jitter=JITTER,
         )
     return scheduler
 
