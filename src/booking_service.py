ae8c505a2b122577ef2c1a67db0e2d0c9494ce15
Clément Walter <clement0walter@gmail.com>
2024-09-03 12:48:22 +0200

    Update to match new version

--- src/booking_service.py
+++ src/booking_service.py
@@ -198,18 +198,15 @@ class BookingService:
             BOOKING_URL,
             params={"page": "reservation", "view": "return_reservation_captcha"},
         )
-        solver = TwoCaptcha(CAPTCHA_API_KEY)
-        response = self.request("get", CAPTCHA_URL)
-        image_file = NamedTemporaryFile(suffix=".png", delete=False)
-        with open(image_file.name, "wb") as f:
-            f.write(response.content)
-        result = solver.normal(image_file.name)
+
+        captcha_check_result = self.solve_captcha(response)
+
         response = self.request(
             "post",
             BOOKING_URL,
             {
-                "j_captcha_response": result["code"],
-                "jcaptchahoneypot": "",
+                "li-antibot-token": captcha_check_result["antibotToken"],
+                "li-antibot-token-code": "",
                 "submitControle": "submit",
             },
             params={"page": "reservation", "action": "reservation_captcha"},
@@ -220,6 +217,129 @@ class BookingService:
             params={"page": "reservation", "view": "reservation_creneau"},
         )
         return response
+
+    def solve_captcha(self, response):
+        antibot_params = re.search(r"LI_ANTIBOT\.loadAntibot\(\[(.*?)\]", response.text, re.DOTALL)
+        if not antibot_params:
+            logger.error("Failed to extract LI_ANTIBOT parameters")
+            return None
+
+        config_items = [item.strip().strip('"') for item in antibot_params.group(1).split(",")]
+        x_li_sp_key = config_items[3]
+
+        captcha_transaction_url = (
+            "https://captcha.liveidentity.com/captcha/public/frontend/api/v3/captchas/transaction"
+        )
+        headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "dnt": "1",
+            "origin": "https://tennis.paris.fr",
+            "referer": "https://tennis.paris.fr/",
+            "sec-ch-ua": '"Not;A=Brand";v="24", "Chromium";v="128"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+            "x-li-js-version": "v4",
+            "x-li-sp-key": x_li_sp_key,
+        }
+        antibot_params = self.session.post(captcha_transaction_url, headers=headers).json()
+
+        captcha_headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/x-www-form-urlencoded",
+            "dnt": "1",
+            "origin": "https://tennis.paris.fr",
+            "referer": "https://tennis.paris.fr/",
+            "sec-ch-ua": '"Not;A=Brand";v="24", "Chromium";v="128"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+            "x-li-antibot-id": antibot_params["antibotId"],
+            "x-li-js-version": "v4",
+            "x-li-request-id": antibot_params["requestId"],
+            "x-li-sp-key": x_li_sp_key,
+        }
+        captcha_url = "https://captcha.liveidentity.com/captcha/public/frontend/api/v3/captchas"
+        captcha_response = self.session.post(
+            captcha_url, headers=captcha_headers, data={"type": "IMAGE", "locale": "FR"}
+        )
+
+        if captcha_response.status_code != 200:
+            logger.error(f"Failed to get captcha. Status code: {captcha_response.status_code}")
+            return None
+
+        captcha_data = captcha_response.json()
+        captcha_image_url = (
+            f"https://captcha.liveidentity.com/captcha{captcha_data['questions'][0]}"
+        )
+        captcha_image_headers = {
+            "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+            "accept-language": "en-US,en;q=0.9",
+            "dnt": "1",
+            "referer": "https://tennis.paris.fr/",
+            "sec-ch-ua": '"Not;A=Brand";v="24", "Chromium";v="128"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "image",
+            "sec-fetch-mode": "no-cors",
+            "sec-fetch-site": "cross-site",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+        }
+        captcha_image_response = self.session.get(captcha_image_url, headers=captcha_image_headers)
+
+        if captcha_image_response.status_code != 200:
+            logger.error(
+                f"Failed to get captcha image. Status code: {captcha_image_response.status_code}"
+            )
+            return None
+
+        # Save the captcha image to a temporary file
+        image_file = NamedTemporaryFile(suffix=".png", delete=False)
+        with open(image_file.name, "wb") as f:
+            f.write(captcha_image_response.content)
+
+        solver = TwoCaptcha(CAPTCHA_API_KEY)
+        result = solver.normal(image_file.name)
+
+        captcha_check_headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/x-www-form-urlencoded",
+            "dnt": "1",
+            "origin": "https://tennis.paris.fr",
+            "referer": "https://tennis.paris.fr/",
+            "sec-ch-ua": '"Not;A=Brand";v="24", "Chromium";v="128"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+            "x-li-js-version": "v4",
+            "x-li-sp-key": x_li_sp_key,
+        }
+        captcha_check_url = (
+            f"https://captcha.liveidentity.com/captcha{captcha_data['captchaValidationUrl']}"
+        )
+        captcha_check_response = self.session.post(
+            captcha_check_url, headers=captcha_check_headers, data={"answer": result["code"]}
+        )
+
+        if captcha_check_response.status_code != 200:
+            logger.error(
+                f"Failed to check captcha. Status code: {captcha_check_response.status_code}"
+            )
+            return None
+
+        return captcha_check_response.json()
 
     def select_court(self, courts, places_order):
         ordered_selection = (
